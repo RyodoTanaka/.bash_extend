cf48d9344655a69142517ddfcfc075718509ff47
Ryodo Tanaka <groadpg@gmail.com>
2019-05-29 17:42:12 +0900

    delete space

--- dfimage.bash
+++ dfimage.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 function _func_dfimage() {
     docker history --no-trunc $1 | tac | tr -s ' ' | cut -d " " -f 5- | sed 's,^/bin/sh -c #(nop) ,,g' | sed 's,^/bin/sh -c,RUN,g' | sed 's, && ,\n  & ,g' | sed 's,\s*[0-9]*[\.]*[0-9]*[kMG]*B\s*$,,g' | head -n -1
 }
