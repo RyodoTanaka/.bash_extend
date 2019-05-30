7de8283002a54041f1bde87027d086d26a106856
RyodoTanaka <groadpg@gmail.com>
2019-05-30 19:07:15 +0900

    update to use bash_history

--- dfimage.bash
+++ dfimage.bash
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-
 function _func_dfimage() {
-    docker history --no-trunc $1 | tac | tr -s ' ' | cut -d " " -f 5- | sed 's,^/bin/sh -c #(nop) ,,g' | sed 's,^/bin/sh -c,RUN,g' | sed 's, && ,\n  & ,g' | sed 's,\s*[0-9]*[\.]*[0-9]*[kMG]*B\s*$,,g' | head -n -1
+    IMAGE="$1"
+    BASE_IMAGE=`docker inspect -f "{{len .RepoDigests }}" $IMAGE`
+    if [ $BASE_IMAGE -eq 0 ]; then
+        BASE_IMAGE=`docker inspect -f "{{ .Config.Image }}" $IMAGE`
+    else
+        BASE_IMAGE=`docker inspect -f "{{index .RepoDigests 0}}" $IMAGE`
+    fi
+    
+    USER="root"
+    if [ -n "$2" ]; then
+        USER="$2"
+    fi
+    
+    # Print base image
+    echo "FROM $BASE_IMAGE"
+
+    # Get bash history commands
+    docker run -it -u $USER $IMAGE cat /$USER/.bash_history | sed 's/\r$//g' > .tmp.txt
+    HEAD_CMD=$(head -n 1 .tmp.txt)
+    sed -i '1d' .tmp.txt
+    TAIL_CMD=$(tail -n 1 .tmp.txt)
+    sed -i '$d' .tmp.txt
+
+    # make commands
+    echo "RUN $HEAD_CMD && \\"
+    cat .tmp.txt | while read cmd; do
+        cmd=`echo $cmd | sed -e 's/apt\-get/apt/g' -e 's/apt/apt\ \-y/g'`
+        if [ "$cmd" = "ls" ]; then
+            continue
+        fi
+        echo "    $cmd && \\"
+    done
+    echo "    $TAIL_CMD"
+
+    # Delete tempolary file
+    rm -rf .tmp.txt
 }
 
 alias dfimage=_func_dfimage
