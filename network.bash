90eee7dac8832546627d67c742c4960371ad2db4
RyodoTanaka <groadpg@gmail.com>
2021-02-03 15:29:18 +0900

    Update to add reference

--- network.bash
+++ network.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function _func_mac2ip() {
+    # Reference
+    # https://stackoverflow.com/questions/13552881/can-i-determine-the-current-ip-from-a-known-mac-address
     ip neighbor | grep $1 | cut -d" " -f1
 }
 
 function _func_ip2mac() {
+    # Reference
+    # https://stackoverflow.com/questions/13552881/can-i-determine-the-current-ip-from-a-known-mac-address
     ip neighbor | grep $1 | cut -d" " -f5   
 }
 
